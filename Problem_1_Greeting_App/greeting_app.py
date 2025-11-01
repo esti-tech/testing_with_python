9f6522467f65e8d2ffe7ab2bb25c5386e03ce8e4
bereket getnet <you@example.com>
2025-11-02 01:16:37 +0300

    Add solution for Problem 6 - Santa Floors

--- Problem_1_Greeting_App/greeting_app.py
+++ Problem_1_Greeting_App/greeting_app.py
@@ -1,12 +1,10 @@
-# your code here
 print("What is your name?")
 name = input()
 print("Hello,", name)
 print("what is your favarite food")
 food=input()
-print("so how old are you" ,(name))
+print("so how old are you",(name))
 age=int(input())
 print(age)
 birth_date=2025-age
-print("ithink your birth is at",(birth_date))
-
+print("I think your birth is at",(birth_date))
